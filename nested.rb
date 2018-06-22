d6a2ac1fc3271210540f1947b587153c8a18908b
Edsel Caprice <caprice@mail.usf.edu>
2018-06-22 13:03:26 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -56,6 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+		 programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
@@ -81,6 +82,7 @@ def adding_matz
         }
      }
 
+programmer_hash[yukihiro_matsumoto: known_for: "Ruby"]
 
 end
 
